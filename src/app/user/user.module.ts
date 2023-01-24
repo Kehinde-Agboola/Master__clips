cb8d360dac1dba4d7dd19120bfa73c601dbc7e41
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-01-24 14:26:49 +0100

    form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
 import { SharedModule  } from "../shared/shared.module";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 @NgModule({
   declarations: [
-    AuthModalComponent
+    AuthModalComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
